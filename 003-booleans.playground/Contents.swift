eb28ef8cd391b9cf15434455335e751fb75612ad
DGV1995 <diegogarciaviana.1995@gmail.com>
2019-07-22 17:45:45 +0200

    003-booleans

---
+++ 003-booleans.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+// Boolean values
+var myBool: Bool = true
+
+let orangesAreOrange = true
+let orangesArePink = false
+
+if orangesArePink {
+    print("Oranges are pink!")
+} else {
+    print("Oranges are orange!")
+}
+
+let i = 3
+
+if i == 3 {
+    print("I have a three")
+}
+
