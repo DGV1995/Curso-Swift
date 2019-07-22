eb1a2d2c91ca24ad3f9e5f1dad3f93155250c934
DGV1995 <diegogarciaviana.1995@gmail.com>
2019-07-22 17:55:39 +0200

    004-tuples

---
+++ 004-tuples.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+// Tuples
+let httpError: (Int, String) = (404, "Error, not found")
+let coordinates: (Int, Int, Int)
+let password: (String, Bool)
+print(httpError)
+
+let (statusCode, statusMessage) = httpError
+print("Status code: \(statusCode), Status message: \(statusMessage)")
+
+let (justStatusCode, _) = httpError
+print("Status code: \(justStatusCode)")
+
+print("The first part is: \(httpError.0), and the second one is: \(httpError.1)")
+
+let http200Status = (statusCode: 200, description: "OK")
+print(http200Status.statusCode)
+print(http200Status.description)
+
+
+
