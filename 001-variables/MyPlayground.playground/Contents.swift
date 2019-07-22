97ec00ccd9806ab6fdbd67935ba7abe9aa6789b8
DGV1995 <diegogarciaviana.1995@gmail.com>
2019-07-22 16:54:33 +0200

    001-variables

--- 001-variables/MyPlayground.playground/Contents.swift
+++ 001-variables/MyPlayground.playground/Contents.swift
@@ -3,5 +3,32 @@ import Cocoa
 // Constant (inmutable)
 let max_number_logins = 10
 
-// Variable
+// Variable (mutable)
 var current_logging_attemp = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
+let language = "Swift"
+
+var welcome_message: String
+welcome_message = "hello"
+
+print(welcome_message)
+
+welcome_message = "Hola"
+print(welcome_message)
+
+//print("Me acaban de saludar diciendo: " + welcome_message)
+print("Me acaban de saludar diciendo: \(welcome_message)")
+
+var red, green, blue: Double
+
+
+let π = 3.14159265 // Alt+p = π
+
+let 😼 = "cat"
+let 🐶🐭 = "dogmouse"
+
+🐶🐭
+
+
