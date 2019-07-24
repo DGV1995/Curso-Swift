c2d1fd36a35379d4bdb0db885fe436f14a218bce
DGV1995 <diegogarciaviana.1995@gmail.com>
2019-07-24 16:40:07 +0200

    003-If

---
+++ 5 - Flow control/003-If .playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// If else
+var isRaining = true
+
+if isRaining {
+    print("Take the umbrella!")
+}
+
+var temperature = 35
+
+if temperature < 12 {
+    print("It's so cold!")
+} else if temperature <= 25 {
+    print("It is a good temperature")
+} else {
+    print("It's so hot!")
+}
